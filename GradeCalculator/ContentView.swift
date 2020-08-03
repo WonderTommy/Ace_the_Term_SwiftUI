145ce889be7b77079bf490d7b037ff27fb4573ff
Hao Li <hlifor931@gmail.com>
2020-08-02 20:03:37 -0400

    Initial Commit

---
+++ GradeCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradeCalculator
+//
+//  Created by Hao Li on 2020-08-02.
+//  Copyright © 2020 Hao Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
